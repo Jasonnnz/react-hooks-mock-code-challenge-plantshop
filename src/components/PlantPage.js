23e069b0463bd83d26cdab3282a560c879421556
Jason Zhou <jzhou35@buffalo.edu>
2021-02-08 14:00:33 -0500

    Done.

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React,{ useEffect, useState } from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [ plants, setPlants ] = useState([]);
+  const [ query, setQuery ] = useState("");
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(r => r.json())
+    .then(plantArr => {
+      setPlants(plantArr)
+    })
+  }, [])
+
+  function findPlant(e){
+    setQuery(e.target.value)
+  }
+
+  const displayPlant = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(query.toLowerCase())
+  })
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} setPlants={setPlants}/>
+      <Search findPlant={findPlant} query={query}/>
+      <PlantList plants={displayPlant}/>
     </main>
   );
 }
